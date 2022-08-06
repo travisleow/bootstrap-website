544ed67fa959ab964102e7554cc25c3c814eabcd
tleow <travis05leow@gmail.com>
2022-08-06 18:16:39 +0800

    idk changed some stuff, experience page revamped

--- js/parallax.js
+++ js/parallax.js
@@ -6,10 +6,10 @@
 
 "use strict";
 
-const parallax = document.getElementById('parallax')
+const parallax = document.getElementById('parallax');
 
 window.addEventListener('scroll', function ()
 {
-    let offset = window.pageYOffset;
-    parallax.style.backgroundPositionY = offset * 0.8 + "px"
+  let offset = window.pageYOffset;
+  parallax.style.backgroundPositionY = offset * 0.8 + "px"
 });
