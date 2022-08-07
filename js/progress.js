4ea0924e9ad5b1a1b1da87c80243a47912715c48
tleow <travis05leow@gmail.com>
2022-08-07 20:36:26 +0800

    comments on progress.js

--- js/progress.js
+++ js/progress.js
@@ -8,18 +8,24 @@
 
 function calcScrollValue() {
     let scrollProgress = document.getElementById('progress');
+    // Distance scrolled from the top to the page to screen
     let pos = document.documentElement.scrollTop;
+    // Getting the height relative to user's resolution
     let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    // Getting the percentage value
     let scrollValue = Math.round((pos * 100) / calcHeight);
+    // If scroll pos < 100, display nothing
     if (pos > 100) {
         scrollProgress.style.display = 'grid';
     }
     else {
         scrollProgress.style.display = 'none';
     }
+    // Eventlistener that scrolls back to top on click
     scrollProgress.addEventListener('click', function(){
         document.documentElement.scrollTop = 0;
     });
+    // Styling of gradient based on height percentage
     scrollProgress.style.background = `conic-gradient(#87AAAA ${scrollValue}%, #C8E3D4 ${scrollValue}%)`;
 }
 
